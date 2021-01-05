2cec76af446038bb8fe80a76c91d898f2883b41e
eladgugi <56247099+eladgugi@users.noreply.github.com>
2021-01-05 18:10:38 +0200

    Update index.py

--- app/index.py
+++ app/index.py
@@ -3,6 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return "Hello, world from Elad_wow!!!"
+    return "Hello, world from Elad_wow_OMG!!!"
 
 app.run(host="0.0.0.0", port=5000)
